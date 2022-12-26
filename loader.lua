ea21680d711f4b60e18d0e2a966dd3c23b818160
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-26 21:47:13 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,7 +3,7 @@ repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/Ads.lua"))()
 local sec = math.random(1,7)
 local milisec = math.random(1,1000)
-wait(sec . milisec)
+wait(sec)
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/game/bloxfruit.lua"))()
