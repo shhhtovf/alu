4e3b85b2d35e18cb50a9c612e4238cd7f5178ce5
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-27 20:13:51 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,17 +1,11 @@
 
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/Ads.lua"))()
-local sec = math.random(1,7)
-local milisec = math.random(1,1000)
+local sec = math.random(1,3)
 wait(sec)
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/game/bloxfruit.lua"))()
-  game.StarterGui:SetCore("SendNotification", {
-      Icon = "rbxassetid://10845671105";
-      Title = "Alucard Hub", 
-      Text = "Loaded In " .. sec .."." .. milisec .. " Seconds"
-  })
 else
   game.StarterGui:SetCore("SendNotification", {
       Icon = "rbxassetid://10845671105";
