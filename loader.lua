b31db1f27dac6fff8ae3abd9a9fb16705f53ca70
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2022-12-27 21:38:55 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,6 +6,13 @@ wait(sec)
 local PlaceId = game.PlaceId
 if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/game/bloxfruit.lua"))()
+elseif PlaceId == 8396586868 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/game/A0nePieceGame.lua"))()
+  game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Notifiction", 
+      Text = "In Dev!"
+  })
 else
   game.StarterGui:SetCore("SendNotification", {
       Icon = "rbxassetid://10845671105";
