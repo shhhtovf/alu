b610ff42a8bf5f576a75da8aefbdce8bd55ccb86
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2023-01-05 17:34:58 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,24 @@
+local key = _G.Key
+
+local Check = game:HttpGet("https://alucardhubwhitelist.000webhostapp.com/check.php?key="..key)
+
+if Check == "Whitelisted" then
+			game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Key Check", 
+      Text = "Correct Key"
+  })
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/Load.lua"))()
+
+		else
+			game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Key Check", 
+      Text = "Incorrect Key"
+  })
+  game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Key Check", 
+      Text = "Loading Key Gui"
+  })
+		end
