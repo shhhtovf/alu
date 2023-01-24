a4b459fb91130562b63187f78d2db32f4ce6c634
shhhtovf <119938893+shhhtovf@users.noreply.github.com>
2023-01-24 10:48:43 +0700

    Update load kaitun.lua

--- load kaitun.lua
+++ load kaitun.lua
@@ -1,1 +1,24 @@
+if _G.Key then
+else
+	_G.Key = "Fruk"
+end
 
+local key = _G.Key
+
+local Check = game:HttpGet("https://alu.jjtchannel.repl.co/?key="..key)
+
+if Check == "Whitelisted" then
+			game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Key Check", 
+      Text = "Correct Key"
+  })
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/shhhtovf/alu/main/kaitunbf.lua"))()
+
+		else
+			game.StarterGui:SetCore("SendNotification", {
+      Icon = "rbxassetid://10845671105";
+      Title = "Alucard Hub - Key Check", 
+      Text = "Incorrect Key Try Again!"
+  })
+		end
